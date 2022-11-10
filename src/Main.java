46cb61d7d4fbe0489ea642a33ea4e9377e36a3e0
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-11-10 20:20:18 +0700

    Update Main.java
    substract

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args){
 
         Decimal number1 = new Decimal("023", "048");
+        Decimal number2 = new Decimal("123", "048");
 
         System.out.println(number1.add());
+        System.out.println(number2.substract());
+
 
     }
 }
